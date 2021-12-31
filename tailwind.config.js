fed5c31a76ffbe13da3792b7d1c14803d00e614b
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2021-12-31 12:49:48 +0100

    add ibm plex font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
+  theme: {
+    fontFamily: {
+      mono: ['IBM Plex Mono', ...defaultTheme.fontFamily.mono]
+    },
+  },
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
