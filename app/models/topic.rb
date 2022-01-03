ea7eb3eaf17e96cc51ece6abf1e33e94fb0d8ca3
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2022-01-03 16:42:29 +0100

    dont use active job to send topics and votes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,8 +4,8 @@ class Topic < ApplicationRecord
 
   validates :content, presence: { message: "Oops! Looks like you forgot to write a topic"}
 
-  after_create_commit { broadcast_prepend_later_to "topics_stream", locals: { talk: self.talk} }
+  after_create_commit { broadcast_prepend_to "topics_stream", locals: { talk: self.talk} }
 
-  after_create_commit { broadcast_replace_later_to "topic_count_stream", target: "topics_count", partial: "topics/index", 
+  after_create_commit { broadcast_replace_to "topic_count_stream", target: "topics_count", partial: "topics/index", 
     locals: { talk: self.talk } }
 end
