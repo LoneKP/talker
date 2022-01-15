692a57b511a3667b714b9d78f1525fff6d8a71ca
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2022-01-15 23:17:45 +0100

    align new page with more inputs

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -25,7 +25,7 @@ class TalksController < ApplicationController
         format.html { redirect_to talk_url(@talk), notice: "Wonderful. Let's get started!" }
       else
         format.html { render :new, status: :unprocessable_entity }
-        flash.now[:error] = @talk.errors.messages.first.second[0]
+        flash.now[:error] = @talk.errors.messages.values
       end
     end
   end
