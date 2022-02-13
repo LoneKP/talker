a94de083d11881b44accd5bacab971f777549896
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2022-02-13 20:21:57 +0100

    see if visitor is facilitatr from datavalue

--- app/javascript/controllers/facilitator_controller.js
+++ app/javascript/controllers/facilitator_controller.js
@@ -2,15 +2,18 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["Facilitator", "Participant"]
-  static values = { url: String }
-
 
   initialize() {
-    this.evaluateRoles()
+    //this.evaluateRoles()
+    if (document.querySelector("body").dataset.facilitator === "true") {
+      this.FacilitatorTargets.forEach(element => element.classList.remove("hidden"));
+    } else {
+      this.ParticipantTargets.forEach(element => element.classList.remove("hidden"));
+    }
   }
 
   evaluateRoles() {
-    fetch(this.urlValue)
+    fetch(`/talks/${document.querySelector("body").dataset.talkId}/current_visitor`)
     .then(response => response.json())
     .then((visitor) => {
       if (visitor.facilitator === true) {
