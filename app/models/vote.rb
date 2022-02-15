b0d977a470e40ef1eaea6ce6763c5a093c8af4a0
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2022-02-15 20:39:16 +0100

    show star along with votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,9 @@
 class Vote < ApplicationRecord
   belongs_to :topic
 
-  after_create_commit { broadcast_replace_later_to "vote_count_stream", target: ActionView::RecordIdentifier.dom_id(self.topic, :votes_count), partial: "votes/votes", 
-    locals: { topic: self.topic } }
+    after_commit :broadcast_content, on: :create
+
+  def broadcast_content
+    topic.talk.broadcast_content
+  end
 end
