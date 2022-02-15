0cbf98234fad909f3ec2e648334b65b039e4c50c
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2022-02-15 20:10:18 +0100

    add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     fontFamily: {
       mono: ['IBM Plex Mono', ...defaultTheme.fontFamily.mono]
     },
+    extend: {
+      colors: {
+        "blue": "#312783",
+        "blue-dark": "#2B2272",
+        "blue-darker": "#261E68",
+        "red": "#A92F3E",
+        "green": "#027540",
+        "green-dark": "#026336",
+        "green-darker": "#01542E",
+        "beige": "#FDF4DD"
+      }
+    }
   },
   content: [
     './app/views/**/*.html.erb',
